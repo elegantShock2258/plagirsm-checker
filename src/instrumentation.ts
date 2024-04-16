39254b56d7969fa7d8824f3c51acecf60b290d43
Elegant Shock <elegantShock3307@gmail.com>
2024-04-17 03:24:03 +0530

    pura working

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,1 +1,16 @@
-export async function register() {}
+import { spawn } from "child_process";
+// Path to your Python script
+const pythonScriptPath = "src/server/server.py";
+export async function register() {
+  const pythonProcess = spawn("python3", [pythonScriptPath], {
+    detached: false,
+    // stdio: "ignore", // Ignore stdio (standard input/output/error)
+  });
+  pythonProcess.on("error", (err) => {
+    console.error("Failed to start Python Server:", err);
+  });
+
+  // Once the Python process is spawned, it will run independently in the background
+  pythonProcess.unref();
+  console.log("Python Server Started");
+}
